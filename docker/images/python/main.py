5273efa56e6a0c94519ff1733b544aa151e9dcd7
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2024-05-09 15:59:39 +0300

    Add simple python code

--- docker/images/python/main.py
+++ docker/images/python/main.py
@@ -1,1 +1,12 @@
-print("Hello main.py")
+import calendar
+
+"""Простой метод который выводит, календарь на месяц в консоль"""
+
+print('Добро пожаловать в календарь\n')
+
+year = int(input('Пожалуйста введите год: '))
+month = int(input('Введите номер любого месяца: '))
+
+print(calendar.month(year, month))
+
+print('Всего хорошего!')
